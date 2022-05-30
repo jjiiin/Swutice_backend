e425a024d7daad13532f8a250377a2566b20fa9a
jjiiin <wjdwlin4051@gmail.com>
2022-05-30 21:23:41 +0900

    학과 5개 키워드 알림 구현

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -4,13 +4,22 @@ from .scrap_no3 import *
 from .scrap_no4 import *
 from .scrap_no5 import *
 from .scrap_security import *
-
+from .scrap_korean import *
+from .scrap_biz import *
+from .scrap_digitalMedia import*
+from .scrap_libraryInfo import*
+from .scrap_socialWelfare import*
 #학교
 #scraping_no1()#학사
 #scraping_no2()#장학
 #scraping_no3()#행사
 #scraping_no4()#채용/취업
-scraping_no5()#일반/봉사
+#scraping_no5()#일반/봉사
 
 #학과
 #scraping_security()#정보보호학과
+#scraping_korean()#국어국문학과
+#scraping_biz()#경영학과
+#scraping_dm()#디지털미디어학과
+#scraping_libraryInfo()#문헌정보학과
+scraping_socialWelfare()
--- myapp/scrap_digitalMedia.py
+++ myapp/scrap_digitalMedia.py
@@ -1,14 +1,14 @@
+from types import NoneType
 import requests
 from bs4 import BeautifulSoup
 from .fb_read import*
-from .push_fcm_notification import *
 from .db_crud import*
 
-dept='security'
-dept_kr='정보보호학과'
-deptNum=24
-def scraping_security():
-    url="http://security.swu.ac.kr/sub.html?page=department_notice&page1=1&searchKey=&searchValue="
+dept='digitalmedia'
+dept_kr='디지털미디어학과'
+deptNum=23
+def scraping_dm():
+    url="http://dm.swu.ac.kr/%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/%ed%95%99%ea%b3%bc-%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/?pageid=1"
     headers={
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
         "Accept-Language":"ko-KR,ko"}
@@ -17,10 +17,7 @@ def scraping_security():
     soup=BeautifulSoup(res.text,'html.parser',from_encoding="utf-8")
     tbody=soup.find("tbody")
     announcements=tbody.find_all("tr")
-    # if titleGetDB()!="":
-    #     title_before=titleGetDB()[0]
-    # else:
-    #     title_before=""
+    #title_before=""
     title_before=titleGetDB(dept)[0].replace(" ","")
     cnt=0
     count=0
@@ -30,20 +27,22 @@ def scraping_security():
         temp_index=str(temp[0].text).replace("\n","")
         if count == 1:
             title_before=titleGetDB(dept)[0].replace(" ","")
-        if temp_index not in "공지":
-            title_=str(temp[1].text).replace("\n","").replace("새글","").replace(" ","")
-            title=str(temp[1].text).replace("\n","").replace("새글","")
+        if temp_index not in "공지사항":
+            title_=str(temp[1].text).replace("\n","").replace("N","").replace(" ","")
+            title=str(temp[1].text).replace("\n","").replace("N","")
             print("title_crawli"+title_)
             print("title_before"+title_before)
             #sqlite에 저장된 공지(제목)과 크롤링해온 제목 비교하기
             if title_before != title_:
                 cnt+=1
                 print("cnt"+str(cnt))
-                find_link=temp[1].a.attrs["href"]
-                link="http://security.swu.ac.kr/"+find_link
+                find_link=str(temp[1].a.attrs["href"])
+                link="http://dm.swu.ac.kr"+find_link
                 contents=contentExtraction(link)
                 #키워드 리스트랑 비교해서 푸쉬알림 보내기
                 content=title+contents
+                print(link)
+                print(contents)
                 pushNotification(content,link,dept_kr,deptNum)
                 if cnt==1:
                     title_update=title
@@ -67,6 +66,10 @@ def contentExtraction(link):
     res=requests.get(link,headers=headers)
     res.raise_for_status()
     soup=BeautifulSoup(res.text,'html.parser',from_encoding="utf-8")
-    content_text=soup.find("div",{"class":"boardview-content"}).text.replace("\n","")
-    return(str(content_text))
-
+    result=""
+    try:
+        content_text=soup.find("div",{"class":"box_bd_view"}).text.replace("\n","")
+        return(str(content_text))  
+    except Exception as e:
+        print(str(e))
+        return result   
